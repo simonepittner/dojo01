0d41859e5a34485a003073768460495c768926bf
simone pittner <simone.pittner@gmail.com>
2013-03-12 14:29:10 -0300

    Resultado do DOJO 11/03/2013 Emphasys

---
+++ FIZZBUZZ/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FIZZBUZZ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string RetornaFizz(int numero)
+        {
+            if (numero == 2)
+                return "Fizz";
+            else
+                return "Nao Fizz";
+        }
+
+        public string RetornaBuzz(int numero)
+        {
+            
+            if (numero == 3)
+                return "Buzz";
+            else
+                return "Nao Buzz";
+        }
+
+        public string RetornarFizzBuzz(int numero)
+        {
+            if (numero < 0)
+                return "Erro";
+
+            if (numero % 2 == 0)
+                return "Fizz";
+            else
+                return "Buzz";
+        }
+
+    }
+}
---
+++ TESTE_FIZZBUZZ/UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FIZZBUZZ;
+
+namespace TESTE_FIZZBUZZ
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DeveRetornarFizz()
+        {
+            Program p = new Program();
+            Assert.AreEqual("Fizz", p.RetornaFizz(2));
+        }
+
+        [TestMethod]
+        public void NaoDeveRetornarFizz()
+        {
+            Program p = new Program();
+            Assert.AreNotEqual("Fizz", p.RetornaBuzz(3));
+        }
+
+        [TestMethod]
+        public void DeveRetornarBuzz()
+        {
+            Program p = new Program();
+            Assert.AreEqual("Buzz", p.RetornaBuzz(3));
+        }
+
+        [TestMethod]
+        public void NaoDeveRetornarBuzz()
+        {
+            Program p = new Program();
+            Assert.AreNotEqual("Buzz", p.RetornaFizz(1));
+        }
+
+        [TestMethod]
+        public void DeveRetornarFizzQuandoFor2()
+        {
+            Program p = new Program();
+            Assert.AreEqual("Fizz", p.RetornaFizz(2));
+        }
+
+        [TestMethod]
+        public void NaoDeveRetornarFizzQuandoNaoFor2()
+        {
+            Program p = new Program();
+            Assert.AreNotEqual("Fizz", p.RetornaFizz(3));
+        }
+
+        [TestMethod]
+        public void DeveRetornarBuzzQuandoFor3()
+        {
+            Program p = new Program();
+            Assert.AreEqual("Buzz", p.RetornaBuzz(3));
+        }
+
+        [TestMethod]
+        public void NaoDeveRetornarBuzzQuandoNaoFor3()
+        {
+            Program p = new Program();
+            Assert.AreNotEqual("Buzz", p.RetornaBuzz(2));
+        }
+
+        [TestMethod]
+        public void DeveRetornarFizzQuandoForNumeroPar()
+        {
+            Program p = new Program();
+            Assert.AreEqual("Fizz", p.RetornarFizzBuzz(2));
+        }
+        
+        [TestMethod]
+        public void DeveRetornarBuzzQuandoForNumeroImpar()
+        {
+            Program p = new Program();
+            Assert.AreEqual("Buzz", p.RetornarFizzBuzz(3));
+        }
+
+        [TestMethod]
+        public void DeveRetornarErroQuandoNumeroMenorZero()
+        {
+            Program p = new Program();
+            Assert.AreEqual("Erro", p.RetornarFizzBuzz(-1));
+        }
+    }
+}
